b4e109fcd089b9b6b7747c25fc796bc48a3f8e9a
Kynaston <weeyuqikynaston@gmail.com>
2026-01-29 19:58:08 +0800

    a)Changed the name to Pepsi b)Included Greeting line and Goodby line

---
+++ src/main/java/Pepsi.java
@@ -0,0 +1,16 @@
+public class Pepsi {
+    public static void main(String[] args) {
+        String longline = "____________________________________________________________";
+        System.out.println(longline);
+        String logo =
+                " ____  _____ ____  ____ ___ \n"
+                        + "|  _ \\| ____|  _ \\/ ___|_ _|\n"
+                        + "| |_) |  _| | |_) \\___ \\| | \n"
+                        + "|  __/| |___|  __/ ___) | | \n"
+                        + "|_|   |_____|_|   |____/___|\n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Pepsi\n" + "What can I do for you?\n" + longline);
+        System.out.println("Bye. Hope to see you again soon!\n" + longline);
+
+    }
+}
