d36c6442ad6dd3889ec4664f7517ac8551b97594
Kynaston <weeyuqikynaston@gmail.com>
2026-01-29 20:02:12 +0800

    c)removed concat and used a text block instead for the printing of "PEPSI"

--- src/main/java/Pepsi.java
+++ src/main/java/Pepsi.java
@@ -2,12 +2,13 @@ public class Pepsi {
     public static void main(String[] args) {
         String longline = "____________________________________________________________";
         System.out.println(longline);
-        String logo =
-                " ____  _____ ____  ____ ___ \n"
-                        + "|  _ \\| ____|  _ \\/ ___|_ _|\n"
-                        + "| |_) |  _| | |_) \\___ \\| | \n"
-                        + "|  __/| |___|  __/ ___) | | \n"
-                        + "|_|   |_____|_|   |____/___|\n";
+        String logo = """
+         ____  _____ ____  ____ ___
+        |  _ \\| ____|  _ \\/ ___|_ _|
+        | |_) |  _| | |_) \\___ \\| |
+        |  __/| |___|  __/ ___) | |
+        |_|   |_____|_|   |____/___|
+        """;
         System.out.println(logo);
         System.out.println("Hello! I'm Pepsi\n" + "What can I do for you?\n" + longline);
         System.out.println("Bye. Hope to see you again soon!\n" + longline);
