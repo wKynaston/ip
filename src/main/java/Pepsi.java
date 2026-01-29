73b684e5eb304717450fc2be17dd29f1e87265b3
Kynaston <weeyuqikynaston@gmail.com>
2026-01-29 20:44:43 +0800

    -made longline a global variable and added a "\n" at the back so that it will move down one line when outputting the switch cases line print ling -inported util.Scanner so that the program will able to take in inputs from the user -created a while loop to run infinitely and created 2 variables one is the input which stores the users input and a boolean variable keepGoing which depneding on its value can break out of the while loop or continue asking for input -made a boolean function "repeat" which returns false to main if the user inputs "bye" and true otherwise. Used a if statement as there is only two conditions as of now

--- src/main/java/Pepsi.java
+++ src/main/java/Pepsi.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Pepsi {
+    public static final String longline = "____________________________________________________________\n";
+    public static boolean repeat(String input){
+        if(input.toLowerCase().equals("bye")) {
+            System.out.println(longline + "Bye. Hope to see you again soon!\n" + longline);
+            return false;
+        }
+        else{
+            System.out.println(longline + input + "\n" + longline);
+            return true;
+        }
+    }
     public static void main(String[] args) {
-        String longline = "____________________________________________________________";
+
         System.out.println(longline);
         String logo = """
          ____  _____ ____  ____ ___
@@ -11,7 +24,17 @@ public class Pepsi {
         """;
         System.out.println(logo);
         System.out.println("Hello! I'm Pepsi\n" + "What can I do for you?\n" + longline);
-        System.out.println("Bye. Hope to see you again soon!\n" + longline);
+        //Create an object called in that can read input typed by the user from the keyboard.
+        Scanner in = new Scanner(System.in);
+        while (true){ //this line will keep running no matter what
+            String input = in.nextLine(); //This will wait for user input
+            boolean keepGoing = repeat(input);
+            if(!keepGoing){
+                //if repeat returns false will break the while loop and exit
+                break;
+            }
+        }
 
     }
+
 }
